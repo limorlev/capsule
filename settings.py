3dc3e5ff7b4f3fbec741d36cfe15f094f34f25c1
Yaniv Harpaz <yaniv.harpaz@gmail.com>
2019-08-16 18:59:32 +0300

    settings file

---
+++ settings.py
@@ -0,0 +1,17 @@
+# prod
+# source_path = r'\\clalit\dfs$\Integration\Capsula'
+# dest_path = r'\\clalit\dfs$\OfekDocs\Capsula'
+# archive_duplicates_path = r'\\clalit\dfs$\OfekDocs\Capsula\Archive_Duplicates'
+# archive_error_path = r'\\clalit\dfs$\OfekDocs\Capsula\Archive_Error'
+
+# test
+# source_path = r'H:\CapsulaArchive_TEMP\BatchProcess\Output\others'
+# dest_path = 'C:\capsula'
+# archive_duplicates_path = r'C:\capsula\Archive_Duplicates'
+# archive_error_path = r'C:\capsula\Archive_Error'
+
+# home
+source_path = r'T:\Users\yaniv\Dropbox\Dropbox\Limor_Laptop\capsula'
+dest_path = r'T:\Users\yaniv\Dropbox\Dropbox\Limor_Laptop\capsula\dest'
+archive_duplicates_path = r'T:\Users\yaniv\Dropbox\Dropbox\Limor_Laptop\capsula\Archive_Duplicates'
+archive_error_path = r'T:\Users\yaniv\Dropbox\Dropbox\Limor_Laptop\capsula\Archive_Error'
